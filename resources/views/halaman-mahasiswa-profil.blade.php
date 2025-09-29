f47ba9e17d333bbf73a63deeb09a679b77cd5bf8
AnugrahGani <anugrah24ti@mahasiswa.pcr.ac.id>
2025-09-29 14:00:26 +0700

    commit

---
+++ resources/views/halaman-mahasiswa-profil.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Selmat Datang!</h1>
+    <h2>Ini Halaman Profil Mahasiswa</h2>
+</body>
+</html>
