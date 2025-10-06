0b4306d8cc51d7a4eb8c5086a07be6661eaab0fd
AbiyyuNauval <abiyyu24ti@mahasiswa.pcr.ac.id>
2025-10-06 14:41:08 +0700

    Submit Form Pertanyaan

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+
+use App\Http\Controllers\QuestionController;
+Route::post('question/store', [QuestionController::class, 'store'])
+->name('question.store');
 
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
@@ -26,4 +31,7 @@ Route::get('/about', function () {
     return view('halaman-about');
 });
 
+Route::get('/home', [HomeController::class, 'index']);
 //hahahahah
+
+
